746cb223440161a8e460a9de868b0c160b5ea913
MohamadAlammar <99723037+MohamadAlammar@users.noreply.github.com>
2024-10-08 13:53:36 +0300

    Done

--- app/components/About/AboutHero.tsx
+++ app/components/About/AboutHero.tsx
@@ -8,13 +8,13 @@ const AboutHero = () => {
         backgroundImage: 'url(/images/back1.png)', // Update the path as needed
       }}
     >
-      <div className="text-center   ">
+      <div className="text-center">
         {/* Set font size to 102px only on large screens and adjust for smaller screens */}
-        <h2 className="text-[36px] md:text-[80px] lg:text-[102px] mb-5 md:mb-0 ">
+        <h2 className="text-[36px] md:text-[80px] lg:text-[102px] mb-7 md:mb-3 ">
           WE KEEP AI SIMPLE
         </h2>
 
-        <p className="text-xs sm:text-sm md:text-base lg:text-lg w-[90%] md:w-[60%] lg:w-[40%] mx-auto">
+        <p className="text-xl md:text-3xl  w-[90%] md:w-[60%] lg:w-[50%] mx-auto px-5">
           We help companies effectively leverage AI by translating abstract concepts into actionable, step-by-step plans and implementations that drive real business value.
         </p>
       </div>
