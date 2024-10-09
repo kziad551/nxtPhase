7dd9c6963328fd52ef1b724c088a54426abf736c
MohamadAlammar <99723037+MohamadAlammar@users.noreply.github.com>
2024-10-09 11:41:38 +0300

    Done

--- app/components/Home/CalendarHome.tsx
+++ app/components/Home/CalendarHome.tsx
@@ -1,26 +1,34 @@
-"use client"; 
-import React from 'react';
-
+"use client";
+import React, { useState } from 'react';
 
 const CalendarHome: React.FC = () => {
-  
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoad = () => {
+    setIsLoading(false);
+  };
 
   return (
-    <div id="bookaCall" className="max-w-7xl mx-auto px-4 mt-12 ">
+    <div id="bookaCall" className="max-w-7xl mx-auto px-4 mt-12">
       <h2 className="text-[28px] sm:text-[28px] md:text-[57px]">Interested in learning more?</h2>
       <h2 className="text-[20px] sm:text-[20px] md:text-4xl mb-8">Schedule time with our CEO below.</h2>
 
-       
-      <iframe 
-        src="https://calendly.com/koen-nxtphase/30min?embed_domain=yourdomain.com&embed_type=Inline?hide_gdpr_banner=1" 
-        width="100%" 
-        height="700px" 
-        style={{ minWidth: '320px', height: '700px', }} 
+      {isLoading && (
+  <div className="flex flex-col justify-center items-center h-[700px]">
+    <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-gray-900 mb-4"></div>
+    <p className="text-gray-700 text-lg">Loading calendar...</p>
+  </div>
+)}
+
+
+      <iframe
+        src="https://calendly.com/koen-nxtphase/30min?embed_domain=yourdomain.com&embed_type=Inline?hide_gdpr_banner=1"
+        width="100%"
+        height="700px"
+        style={{ minWidth: '320px', height: '700px', display: isLoading ? 'none' : 'block' }}
         title="Calendly Schedule"
+        onLoad={handleLoad}
       />
-      
-  
-     
     </div>
   );
 };
